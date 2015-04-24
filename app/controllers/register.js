34f831c3a124caddbea74c95b5c8a8f0c6138d3e
Beth Hoskins <bhoskins@charter.net>
2015-04-24 17:57:16 -0400

    clean up

--- app/controllers/register.js
+++ app/controllers/register.js
@@ -3,16 +3,6 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   actions: {
-    // register: function(){
-    //   var self = this;
-    //   var user = this.get('model');
-    //   console.log(user);
-    //   user.username = user.email;
-    //   user.save().then(function(){
-    //     self.get('session').authenticate('authenticator:parse-email', user);
-    //   });
-    //     this.transitionToRoute('dayPlan');
-    // }
 
     register: function(){
       var user = this.getProperties('firstName', 'lastName', 'username', 'password');

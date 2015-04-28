e42aba7a0febbf5fa20adef26d8a71bc08c27a10
Beth Hoskins <bhoskins@charter.net>
2015-04-28 15:08:03 -0400

    added simple auth to additional routes

--- app/routes/calendar.js
+++ app/routes/calendar.js
@@ -1,6 +1,7 @@
+import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixin';
 import Ember from 'ember';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(AuthenticatedRouteMixin, {
     model: function(params){
     return this.store.find('dayPlan', params.dayPlan_id);
   }
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,7 @@
+import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixin';
 import Ember from 'ember';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(AuthenticatedRouteMixin, {
   beforeModel: function(){
     var today = new Date();
     var dateString = today.getUTCFullYear() + "-" + ("0" + (today.getUTCMonth() + 1)).slice(-2) + "-" + ("0" + (today.getUTCDate())).slice(-2);
